e40c25a904c4318ca787af97f3ded4a71d06e87d
PraneethJain <praneethjain005@gmail.com>
2023-01-05 13:41:10 +0530

    Added noise

--- Binary Tree/tree.c
+++ Binary Tree/tree.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "allegro.h"
+
+float random_num()
+{
+    return (float)rand() / (float)(RAND_MAX)-0.5;
+}
 
 const float del_theta = 0.4;
 const float del_g = 0.1;
 const float length_factor = 0.8;
-const float thickness_factor = 0.9;
+const float thickness_factor = 0.825;
 
-void draw_line(int x, int y, float length, float angle, float thickness, float g)
+void draw_line(int x, int y, float length, float angle, float thickness, int level)
 {
-    if (length < 10)
+    if (level == 14)
         return;
     float end_x = x - length * sin(angle);
     float end_y = y - length * cos(angle);
-    al_draw_line(x, y, end_x, end_y, al_map_rgb_f(0, g, 0), thickness);
-    draw_line(end_x, end_y, length * length_factor, angle + del_theta, thickness * thickness_factor, g + del_g);
-    draw_line(end_x, end_y, length * length_factor, angle - del_theta, thickness * thickness_factor, g + del_g);
+    al_draw_line(x, y, end_x, end_y, level <= 10 ? al_map_rgb(97, 59, 22) : al_map_rgb(12, 174, 91), thickness);
+    draw_line(end_x, end_y, length * length_factor, angle + del_theta + random_num() / 4, thickness * thickness_factor, level + 1);
+    draw_line(end_x, end_y, length * length_factor, angle - del_theta + random_num() / 4, thickness * thickness_factor, level + 1);
     al_flip_display();
 }
 
 void rec_drawing()
 {
-    draw_line(WIDTH / 2, HEIGHT, 150, 0, 5, 0);
+    draw_line(WIDTH / 2, HEIGHT, 150, 0, 9, 0);
 }
 
 int main()
